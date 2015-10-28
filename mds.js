4bfe15968ad37236a9df8fc00e65e950c3e0632d
donasaur <donhmai@gmail.com>
2015-10-28 16:53:42 -0700

    CommonJS version of mds

--- mds.js
+++ mds.js
@@ -1,3 +1,16 @@
+
+var d3 = require('d3');
+var numeric = require('numeric');
+
+d3.selection.prototype.moveToFront = function() {
+    return this.each(function(){
+        this.parentNode.appendChild(this);
+    });
+};
+
+var mds = {};
+exports = mds;
+
 (function(mds) {
     "use strict";
     /// given a matrix of distances between some points, returns the
@@ -35,8 +48,8 @@
     mds.drawD3ScatterPlot = function(element, xPos, yPos, labels, params) {
         params = params || {};
         var padding = params.padding || 32,
-            w = params.w || Math.min(720, document.documentElement.clientWidth - padding),
-            h = params.h || w,
+            w = params.w || document.documentElement.clientWidth - padding,
+            h = params.h || document.documentElement.clientHeight - padding,
             xDomain = [Math.min.apply(null, xPos),
                        Math.max.apply(null, xPos)],
             yDomain = [Math.max.apply(null, yPos),
@@ -72,31 +85,74 @@
                 .attr("width", w)
                 .attr("height", h);
 
-        svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(0," + (h - padding + 2*pointRadius) + ")")
-            .call(xAxis);
+        if (params.includeAxis) {
+            svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(0," + (h - padding + 2*pointRadius) + ")")
+                .call(xAxis);
 
-        svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(" + (padding - 2*pointRadius) + ",0)")
-            .call(yAxis);
+            svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(" + (padding - 2*pointRadius) + ",0)")
+                .call(yAxis);
+        }
 
         var nodes = svg.selectAll("circle")
             .data(labels)
             .enter()
             .append("g");
-        
-        nodes.append("circle")
-            .attr("r", pointRadius)
-            .attr("cx", function(d, i) { return xScale(xPos[i]); })
-            .attr("cy", function(d, i) { return yScale(yPos[i]); });
 
-        nodes.append("text")
-            .attr("text-anchor", "middle")
-            .text(function(d) { return d; })
-            .attr("x", function(d, i) { return xScale(xPos[i]); })
-            .attr("y", function(d, i) { return yScale(yPos[i]) - 2 *pointRadius; });
+        if (params.showImage) {
+            var borderStyle = "fill:rgb(255,255,255);stroke-width:4;stroke:rgb(255,255,0)";
+            var hiddenBorderStyle = "visibility:hidden;" + borderStyle;
+            var rectSize = 80;
+            var imageSize = 50;
+            nodes.append("rect")
+                .attr("class", "dataimageborder")
+                .attr("width", rectSize)
+                .attr("height", rectSize)
+                .attr("x", function(d, i) { return xScale(xPos[i]); })
+                .attr("y", function(d, i) { return yScale(yPos[i]); })
+                .attr("style", hiddenBorderStyle);
+            nodes.append("image")
+                .attr("class", "dataimage")
+                .attr("xlink:href", function(d) { return (d.image)? d.image:d; })
+                .attr("width", imageSize)
+                .attr("height", imageSize)
+                .attr("x", function(d, i) { return xScale(xPos[i]); })
+                .attr("y", function(d, i) { return yScale(yPos[i]); })
+                .on("mouseover", function(){
+                    d3.select(this).attr("width", rectSize).attr("height", rectSize);
+                    d3.select(this.parentNode).moveToFront();
+                    d3.select(this.parentNode).select("rect").attr("style",borderStyle);
+                })
+                .on("mouseout", function(){
+                    d3.select(this).attr("width", imageSize).attr("height", imageSize);
+                    d3.select(this.parentNode).select("rect").attr("style",hiddenBorderStyle);
+                })
+                .on("click", function(d,i){
+                    if (d.onclick) {
+                        d.onclick()
+                    }
+                });
+        } else {
+            nodes.append("circle")
+                .attr("class", "datapoint")
+                .attr("r", pointRadius)
+                .attr("cx", function(d, i) { return xScale(xPos[i]); })
+                .attr("cy", function(d, i) { return yScale(yPos[i]); })
+                .on("click", function(d,i){
+                        if (d.onclick) {
+                            d.onclick()
+                        }
+                    });
+
+            nodes.append("text")
+                .attr("class", "label")
+                .attr("text-anchor", "middle")
+                .text(function(d) { return (d.label)? d.label:d; })
+                .attr("x", function(d, i) { return xScale(xPos[i]); })
+                .attr("y", function(d, i) { return yScale(yPos[i]) - 2 *pointRadius; });
+        }
     };
-}(window.mds = window.mds || {}));
-
+}(mds));
